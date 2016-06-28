79e0ae0495d01747e441c14e0c136ee9b6af2d17
Ni Tianzhen <nitianzhen1210@gmail.com>
2016-06-28 21:40:59 +0800

    Feature: New functions

--- scientific indicator.py
+++ scientific indicator.py
@@ -1,3 +1,5 @@
+import math
+
 f = open('0US1.MSFT_160101_160531.txt','r')
 content = []
 for line in f:
@@ -32,6 +34,65 @@ class minute:
                 theta = 1
             else:
                 theta = -1
+            self.OBV = temp_data.get_OBV() + theta * VOL
+            return self.OBV
+        def get_MA5(self):
+            temp_data = data[self.index - 4:self.index + 1]
+            s = 0.0
+            for i in temp_data:
+                s += i.closeP
+            s /= 5
+            return s
+        def get_MA6(self):
+            temp_data = data[self.index - 5:self.index + 1]
+            s = 0.0
+            for i in temp_data:
+                s += i.closeP
+            s /= 6
+            return s
+        def get_BIAS6(self):
+            return (float(self.closeP) - self.get_MA6())/self.get_MA6()
+        def get_PSY12(self):
+            temp_data = data[self.index - n + 1:self.index + 1]
+            A = 0
+            for i in temp_data:
+                if i.closeP > i.openP:
+                    A += 1
+            return A / 12.0
+        def get_SY:
+            temp_data = data[self.index - 1]
+            return (math.log(self.closeP) - math.log(temp_data.closeP)) * 100.0
+        def get_ASY5:
+            temp_data = data[self.index - 4:self.index + 1]
+            s = 0.0
+            for i in temp_data:
+                s += i.get_SY()
+            s /= 5
+            return s
+        def get_ASY4:
+            temp_data = data[self.index - 3:self.index + 1]
+            s = 0.0
+            for i in temp_data:
+                s += i.get_SY()
+            s /= 4
+            return s
+        def get_ASY3:
+            temp_data = data[self.index - 2:self.index + 1]
+            s = 0.0
+            for i in temp_data:
+                s += i.get_SY()
+            s /= 3
+            return s
+        def get_ASY2:
+            temp_data = data[self.index - 1:self.index + 1]
+            s = 0.0
+            for i in temp_data:
+                s += i.get_SY()
+            s /= 2
+            return s
+        def get_ASY1:
+            temp_data = data[self.index - 1]
+            return temp_data.get_SY()
 
 
 data = []
