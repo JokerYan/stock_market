b9d3e270fa0f579b9998910ab3d17479c2d7358c
Ni Tianzhen <nitianzhen1210@gmail.com>
2016-06-28 21:02:41 +0800

    Bugfix: Class name minute -> bundle

--- scientific indicator.py
+++ scientific indicator.py
@@ -8,7 +8,7 @@ for i in range(len(content)):
     content[i] = content[i].split(';')
 n = 5
 t = 60
-class minute:
+class bundle:
     def __init__(self,name,date,time,openP,highP,lowP,closeP,VOL,index):
         self.name = str(name)
         self.date = str(date)
@@ -18,9 +18,10 @@ class minute:
         self.lowP = float(lowP)
         self.closeP = float(closeP)
         self.VOL = int(VOL[:-1])
-        self.sequence = int(self.date+self.time)
+        self.sequence = int(self.date + self.time)
         self.index = int(index)
-        def get_OBV(self, temp_data = data[self.index - 1]):
+        def get_OBV(self):
+            temp_data = data[self.index - 1]
             if temp_data.closeP > self.closeP:
                 theta = 1
             else:
@@ -28,7 +29,7 @@ class minute:
 
 data = []
 for i in range(len(content)):
-    data.append(minute(content[i][0],content[i][2],content[i][3],\
+    data.append(bundle(content[i][0],content[i][2],content[i][3],\
                        content[i][4],content[i][5],content[i][6],\
                        content[i][7],content[i][8],i))
 
