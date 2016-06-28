36dadf5223f4a686b1354d596b2da5cf572420c3
JokerYan <770792058@qq.com>
2016-06-28 21:07:46 +0800

    hahaha

--- scientific indicator.py
+++ scientific indicator.py
@@ -8,7 +8,7 @@ for i in range(len(content)):
     content[i] = content[i].split(';')
 n = 5
 t = 60
-class bundle:
+class minute:
     def __init__(self,name,date,time,openP,highP,lowP,closeP,VOL,index):
         self.name = str(name)
         self.date = str(date)
@@ -18,8 +18,14 @@ class bundle:
         self.lowP = float(lowP)
         self.closeP = float(closeP)
         self.VOL = int(VOL[:-1])
-        self.sequence = int(self.date + self.time)
+        self.sequence = int(self.date+self.time)
         self.index = int(index)
+
+
+        def stochastic_K(self):
+            pass
+
+
         def get_OBV(self):
             temp_data = data[self.index - 1]
             if temp_data.closeP > self.closeP:
@@ -27,9 +33,10 @@ class bundle:
             else:
                 theta = -1
 
+
 data = []
 for i in range(len(content)):
-    data.append(bundle(content[i][0],content[i][2],content[i][3],\
+    data.append(minute(content[i][0],content[i][2],content[i][3],\
                        content[i][4],content[i][5],content[i][6],\
                        content[i][7],content[i][8],i))
 
